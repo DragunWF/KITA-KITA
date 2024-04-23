3d1a59897ba14e416347124697ebea9f9803661b
papsykle <toramgarcia@gmail.com>
2024-04-23 17:41:01 +0800

    sign up event

--- app/src/main/java/com/example/kita_kita/LandingPage.java
+++ app/src/main/java/com/example/kita_kita/LandingPage.java
@@ -1,6 +1,8 @@
 package com.example.kita_kita;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LandingPage extends AppCompatActivity {
-
+    Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing_page);
+
+        signUpBtn = findViewById(R.id.signUpFormBtn);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signUpBtn.setOnClickListener(v -> {
+            startActivity(new Intent(LandingPage.this, SignUpForm.class));
         });
     }
 }
