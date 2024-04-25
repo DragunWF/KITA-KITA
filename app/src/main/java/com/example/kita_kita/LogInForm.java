a912c186f2b5972728495177a35497f57b978e9e
dragunwf <plarisanmarc2@gmail.com>
2024-04-25 22:02:13 +0800

    Update login form java and layout activity

--- app/src/main/java/com/example/kita_kita/LogInForm.java
+++ app/src/main/java/com/example/kita_kita/LogInForm.java
@@ -1,6 +1,8 @@
 package com.example.kita_kita;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.kita_kita.utils.Utils;
+
 public class LogInForm extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,22 @@ public class LogInForm extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.inputUsernameLogin);
+        password = findViewById(R.id.inputPasswordLogin);
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(v -> {
+            if (Utils.isEmpty(username)) {
+                toast("Username cannot be empty!");
+            } else if (Utils.isEmpty(password)) {
+                toast("Password cannot be empty!");
+            } else {
+                // TODO: Direct user to the main activity
+            }
+        });
+    }
+
+    private void toast(String message) {
+        Utils.toast(this, message);
     }
 }
