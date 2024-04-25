a044c0c07fee62b2520dc2b49f8411b2907514f8
papsykle <toramgarcia@gmail.com>
2024-04-25 21:20:20 +0800

    this one

---
+++ app/src/main/java/Data/Database.java
@@ -0,0 +1,14 @@
+package Data;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Database {
+    private DatabaseReference mDatabase;
+    public void writeRegisterData(String username, String password, String firstName, String lastName, int age, int contact, String email, String position) {
+        mDatabase = FirebaseDatabase.getInstance("https://mobile-projects-a5f6f-default-rtdb.firebaseio.com/").getReference();
+        Account user = new Account(username, password, firstName,
+                                   lastName, age, contact, email, position);
+        mDatabase.child("user").child(user.getUsername()).setValue(user);
+    }
+}
